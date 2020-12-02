fa5fed5a7cdadcf42a6db4b0144bd62344ef27c2
shenng <shenng@gmail.com>
2020-12-02 23:44:32 +0200

    начало 5.10

--- pixi/main.js
+++ pixi/main.js
@@ -1,56 +1,74 @@
-const app = new PIXI.Application();
+const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
 
-let bol = false;
-
-// create a texture from an image path
-const texture = PIXI.Texture.from('img/kam.png');
-
-// create a second texture
-const secondTexture = PIXI.Texture.from('img/510.jpg');
-
-// create a new Sprite using the texture
-const dude = new PIXI.Sprite(texture);
-
-// center the sprites anchor point
-dude.anchor.set(0.5);
-
-// move the sprite to the center of the screen
-dude.x = app.screen.width / 2;
-dude.y = app.screen.height / 2;
-dude.width = 350;
-dude.height = 400;
-
-app.stage.addChild(dude);
-
-// make the sprite interactive
-dude.interactive = true;
-dude.buttonMode = true;
-
-dude.on('pointertap', () => {
-    bol = !bol;
-    if (bol) {
-        dude.texture = secondTexture;
-        dude.width = 550;
-        dude.height = 300;
-    } else {
-        dude.texture = texture;
-        dude.width = 350;
-        dude.height = 400;
-
-    }
+const basicTextStyle = new PIXI.TextStyle({
+  fontFamily: 'Arial',
+  fontSize: 36,
+  fontStyle: 'italic',
+  fontWeight: 'bold',
+  fill: ['#ffffff', '#00ff99'], // gradient
+  stroke: '#4a1850',
+  strokeThickness: 5,
+  dropShadow: true,
+  dropShadowColor: '#000000',
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+  wordWrap: true,
+  wordWrapWidth: 440,
+  lineJoin: 'round'
 });
-
-
-const basicText = new PIXI.Text('кликни чтоб познать дзен', { fill: ['#fff', 'red'], });
+const basicText = new PIXI.Text('Кликай, чтоб переродить Балашова', basicTextStyle);
 basicText.x = 50;
 basicText.y = 50;
-
 app.stage.addChild(basicText);
 
+// Scale mode for all textures, will retain pixelation
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+const texture = PIXI.Texture.from('img/bb.png');
+const secondTexture = PIXI.Texture.from('img/b.png');
+const sprite = PIXI.Sprite.from(texture);
+
+// Set the initial position
+sprite.anchor.set(0.5);
+sprite.x = app.screen.width / 2;
+sprite.y = app.screen.height / 2;
+sprite.scale.x = 3;
+sprite.scale.y = 3;
+
+// Opt-in to interactivity
+sprite.interactive = true;
+
+// Shows hand cursor
+sprite.buttonMode = true;
+
+// Pointers normalize touch and mouse
+sprite.on('pointerdown', onClick);
+
+// Alternatively, use the mouse & touch events:
+// sprite.on('click', onClick); // mouse-only
+// sprite.on('tap', onClick); // touch-only
+
+app.stage.addChild(sprite);
+
+function onClick() {
+  const delta = 0.65
+    if (sprite.scale.x*delta > 1) {
+      sprite.scale.x *= delta;
+      sprite.scale.y *= delta;
+    } else {
+      sprite.texture = secondTexture;
+      sprite.scale.x = 0.7;
+      sprite.scale.y = 0.7;
+      sprite.anchor.set(0.5, 1);
+      sprite.x = app.screen.width / 2;
+      sprite.y = app.screen.height;
+
+      basicText.text = 'Огонь! Теперь можно и в бой!';
+    }
+
+}
 
 
-app.ticker.add(() => {
-    // just for fun, let's rotate mr rabbit a little
-    dude.rotation += 0.001;
-});
+
