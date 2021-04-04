2af48ac9508b0d2261fe532f4a8dc1793472c35f
shenng <shenng4@gmail.com>
2021-04-04 22:16:36 +0300

    mono_timer path urls

--- mono_timer/precache-manifest.935d5ceb69d2e312718db9ddb4485fc7.js
+++ mono_timer/precache-manifest.935d5ceb69d2e312718db9ddb4485fc7.js
@@ -5,26 +5,26 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "73b71bac8d5cb9061b39",
-    "url": "/static/css/2.09ae8418.chunk.css"
+    "url": "mono_timer/static/css/2.09ae8418.chunk.css"
   },
   {
     "revision": "5877bcad64f0db2a8872",
-    "url": "/static/css/main.4fbb4699.chunk.css"
+    "url": "mono_timer/static/css/main.4fbb4699.chunk.css"
   },
   {
     "revision": "73b71bac8d5cb9061b39",
-    "url": "/static/js/2.a4b9fe95.chunk.js"
+    "url": "mono_timer/static/js/2.a4b9fe95.chunk.js"
   },
   {
     "revision": "5877bcad64f0db2a8872",
-    "url": "/static/js/main.7f41fe75.chunk.js"
+    "url": "mono_timer/static/js/main.7f41fe75.chunk.js"
   },
   {
     "revision": "30435d9de1bbc1379411",
-    "url": "/static/js/runtime-main.729b5d94.js"
+    "url": "mono_timer/static/js/runtime-main.729b5d94.js"
   },
   {
     "revision": "043a959826cfc7e2107eda8dcf1ec0a9",
-    "url": "/static/media/logo.043a9598.svg"
+    "url": "mono_timer/static/media/logo.043a9598.svg"
   }
 ]);
