5ba1449dfa612e7020e698cb0cf105227b8ba527
shenng <shenng4@gmail.com>
2021-04-04 22:05:01 +0300

    monoTimer v0.1

---
+++ monoTimer/precache-manifest.935d5ceb69d2e312718db9ddb4485fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f7223122a8cc4c976ce62dc80807a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b71bac8d5cb9061b39",
+    "url": "/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "5877bcad64f0db2a8872",
+    "url": "/static/css/main.4fbb4699.chunk.css"
+  },
+  {
+    "revision": "73b71bac8d5cb9061b39",
+    "url": "/static/js/2.a4b9fe95.chunk.js"
+  },
+  {
+    "revision": "5877bcad64f0db2a8872",
+    "url": "/static/js/main.7f41fe75.chunk.js"
+  },
+  {
+    "revision": "30435d9de1bbc1379411",
+    "url": "/static/js/runtime-main.729b5d94.js"
+  },
+  {
+    "revision": "043a959826cfc7e2107eda8dcf1ec0a9",
+    "url": "/static/media/logo.043a9598.svg"
+  }
+]);
